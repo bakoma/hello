67ab020b3a1f732a355f6aee6ad0ba4b59078cce
BaKoMa <bakoma@bakomacretina.(none)>
2012-09-30 09:56:10 +0400

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hello
+//
+//  Created by BaKoMa on 30.09.12.
+//  Copyright (c) 2012 BaKoMa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
